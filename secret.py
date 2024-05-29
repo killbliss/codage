cf120a3812d0de7338a24873f58ec5a303e24936
killbliss <foadsehla@gmail.com>
2024-05-29 15:45:21 +0200

    coucou

--- secret.py
+++ secret.py
@@ -2,30 +2,15 @@
 code_ = "p+çS#2z8GceHw`Ormè M;%@3g€KRa!^iv1sA,Q9qCIb:NYDhy'/u0oF?éXEkB*jP¨àTxd-_£$(5JWtZl.ùLn=6f4U§&V)7"
 
 
-def encode(string):
-    """
-    Fonction d'encadage
-    Args :
-    string (str): chaine de caractères à encoder
-    Return:
-    str
-
-    ex : encode("Hello, world !") -> 'wH..FQM`Fm.-M^'
-    """
-    pass
+def encode(texte):
+    codage = []
+    for index, char in enumerate(texte):
+        Y = (index + 3) % len(code_)  
+    return ''.join(codage), texte 
 
 
-def decode(string):
-    """
-    Fonction d'encadage
-    Args :
-    string (str): chaine de caractères à décoder
-    Return :
-    str
-
-    ex : decode("wH..FQM`Fm.-M^") -> 'Hello, world !'
-    """
-    pass
+def decode(cd):
+   return texte 
 
 
 if __name__ == "__main__":
